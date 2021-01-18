e75db4656d8b862cfe8049cf1705afb908afa3f7
Anthony Potts <anthonypotts@Anthonys-MacBook-Pro-2.local>
2021-01-18 15:49:29 -0600

    refactored code into one index.js

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const inquirer = require('inquirer');
-const database = require('./db/database');
+// const database = require('./db/database');
+const mysql = require('mysql2');
+const cTable = require('console.table');
+const promise = require('mysql2/promise');
+// require('dotenv').config();
+
+// connect to the database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'ShallowBrown@9',
+    database: 'employees'
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    promptUser();
+});
 
 const promptUser = () => {
     console.log(`
@@ -54,31 +71,101 @@ const promptUser = () => {
         let choice = res.choice;
         switch (choice) {
             case "VIEW_DEPARTMENTS":
-                database.viewDepartments();
+                viewDepartments();
                 break;
             case "VIEW_ROLES":
-                database.viewRoles();
+                viewRoles();
                 break;
             case "VIEW_EMPLOYEES":
-                database.viewEmployees();
+                viewEmployees();
                 break;
             case "ADD_DEPARTMENT":
-                database.addDepartment();
+                addDepartment();
                 break;
             case "ADD_ROLE":
-                database.addRole();
+                addRole();
                 break;
             case "ADD_EMPLOYEE":
-                database.addEmployee();
+                addEmployee();
                 break;
             case "UPDATE_EMPLOYEE_ROLE":
-                database.updateEmployeeRole();
+                updateEmployeeRole();
                 break;
             case "QUIT":
-                database.quit();
+                quit();
                 break;
         }
     })
 }
 
-promptUser();
+
+
+viewDepartments = () => {
+    connection.query('SELECT * FROM department', function(err, res) {
+        if (err) throw err;
+        console.table(res);
+    });
+};
+
+viewRoles = () => {
+    connection.query(
+        'SELECT * FROM role',
+        (err, res) => {
+            if (err) throw err;
+            console.table(res);
+        }
+    );
+}
+
+viewEmployees = () => {
+    connection.query(
+        'SELECT * FROM employee',
+        (err, res) => {
+            if (err) throw err;
+            console.table(res);
+        }
+    );
+}
+
+addDepartment = () => {
+    // connection.query(
+    //     'INSERT INTO department',
+    //     (err, res) => {
+    //         if (err) throw err;
+    //         console.table(res);
+    //     }
+    // );
+}
+
+addRole = () => {
+    // connection.query(
+    //     'INSERT INTO role',
+    //     (err, res) => {
+    //         if (err) throw err;
+    //         console.table(res);
+    //     }
+    // );
+}
+
+addEmployee = ()=> {
+    // connection.query(
+    //     (err, res) => {
+    //         if (err) throw err;
+    //         console.table(res);
+    //     }
+    // );
+}
+
+updateEmployeeRole = () => {
+    // connection.query(
+    //     (err, res) => {
+    //         if(err) throw err;
+    //         console.table(res);
+    //     }
+    // );
+}
+
+quit = () => {
+    connection.end();
+    console.log('Thanks, see you again soon!')
+}
